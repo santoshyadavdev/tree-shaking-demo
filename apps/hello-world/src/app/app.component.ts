1de70dc36f342a48db9b2a7da72632cdb86f0e29
Santosh Yadav <santosh.yadav198613@gmail.com>
2024-12-19 11:45:33 +0100

    add GodModule to import

--- apps/hello-world/src/app/app.component.ts
+++ apps/hello-world/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { GodModuleLibModule } from '@tree-shaking-demo/god-module-lib';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, GodModuleLibModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
