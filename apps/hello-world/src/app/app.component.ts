dd2f48128ca9393088798d5ad96a34202773f753
Santosh Yadav <santosh.yadav198613@gmail.com>
2024-12-20 15:14:00 +0100

    add publishable lib

--- apps/hello-world/src/app/app.component.ts
+++ apps/hello-world/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { ButtonComponent } from '@tree-shaking-demo/god-module-lib';
+import {GridComponent, AccordionComponent} from '@ngx-santosh/ng-publishable';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule, ButtonComponent],
+  imports: [NxWelcomeComponent, RouterModule, ButtonComponent, GridComponent, AccordionComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
